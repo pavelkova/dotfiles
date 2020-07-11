8dbd631a6d9c1c93dd23c62201cfff3109e55573
e.g. pavelka <pav@egpavelka.com>
2020-07-11 00:16:49 -0400

    add fish shell packages

--- xfce+xmonad/.xmonad/lib/Colors.hs
+++ xfce+xmonad/.xmonad/lib/Colors.hs
@@ -2,29 +2,9 @@ module Colors where
 
 -- XMonad modules
 import XMonad
-import XMonad.Prompt
-import XMonad.Hooks.DynamicHooks
-import XMonad.Hooks.DynamicLog
-import XMonad.Layout.Decoration
-import XMonad.Layout.ImageButtonDecoration
-import XMonad.Util.Image
-import XMonad.Util.Loggers
-import XMonad.Util.WorkspaceCompare
 
-wpgTheme =
-  newTheme { themeName        = "wpgTheme"
-           , themeAuthor      = "egp"
-           , themeDescription = "generate custom color themes with wpg"
-           , theme            = def { activeColor         = "{color8}"
-                                    , inactiveColor       = "{color0}"
-                                    , urgentColor         = "{color1}"
-                                    , activeBorderColor   = "{color8}"
-                                    , inactiveBorderColor = "{color0}"
-                                    , urgentBorderColor   = "{color1}"
-                                    , activeTextColor     = "{color15}"
-                                    , inactiveTextColor   = "{color7}"
-                                    , urgentTextColor     = "{color0}"
-                                    , fontName            = "xft:Cantarell:regular:size=10"
-                                    , decoHeight          = 15
-                                    }
-           }
+
+-- wpgtk-defined
+backgroundColor = "{color1}"
+middleColor     = "{color11}"
+foregroundColor = "{color15}"
