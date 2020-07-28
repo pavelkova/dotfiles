32c4f06d47bc71fc65b4cd6e3f29ff49a48d89f9
e.g. pavelka <pav@egpavelka.com>
2020-07-27 22:59:47 -0400

    cleanup diffs

---
+++ fish/.config/fish/functions/em.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function em --description 'edit file in new emacsclient frame'
+	emacsclient -a '' -c $argv;
+end
---
+++ fish/.config/fish/functions/es.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function es --description 'translate word to spanish'
+	crow -t es $argv;
+end
