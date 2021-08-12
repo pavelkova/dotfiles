0253f0b7b206035ec7f891b74435bf71f9fb4a2f
e.g. pavelka <pav@egpavelka.com>
2021-08-12 16:07:32 -0400

    update wpgtk templates for xmonad changes

--- xfce+xmonad/.xmonad/lib/Colors.hs
+++ xfce+xmonad/.xmonad/lib/Colors.hs
@@ -7,5 +7,5 @@ import XMonad
 -- wpgtk-defined
 myBackgroundColor     = "#040b09"
 myForegroundColor     = "#e6e9e9"
-myActiveColor   = "#8c72ad"
-myInactiveColor = "#080e0c"
+myActiveColor         = "#8c72ad"
+myInactiveColor       = "#080e0c"
